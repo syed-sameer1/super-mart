2202a1a2b13e7f35f3cb7411f443926e723e681f
syed-sameer1 <48875785+syed-sameer1@users.noreply.github.com>
2019-06-19 01:58:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let total = 0;
 
 let appleqty = 0;
@@ -9,18 +23,53 @@ let mugqty = 0;
 let roseqty = 0;
 
 
+function keep() {
+    document.querySelector(".black2").style = "display: none";
+    document.querySelector("#body").style = "overflow: show";
+
+    document.querySelector(".mess2").style = "display : none";
+}
+function ok2() {
+    document.querySelector(".black").style = "display: none";
+    document.querySelector("#body").style = "overflow: show";
+
+    document.querySelector(".mess").style = "display : none";
+}
+
+function ok() {
+    document.querySelector(".black3").style = "display: none";
+    document.querySelector("#body").style = "overflow: show";
+
+    document.querySelector(".mess3").style = "display : none";
+}
+
+function buy() {
+    document.querySelector(".black2").style = "display: flex";
+    document.querySelector("#body").style = "overflow: hidden";
+    document.querySelector(".mess2").style = "display : flex";
+
+}
+
 function addapple() {
-    appleqty++;
-    console.log(appleqty)
-    document.querySelector('#apple').value = appleqty;
-    total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
-    document.getElementById('totalbill').innerHTML = total;
-    console.log(total);
-    // if (document.querySelector(#wallet) < total){
-    //     alert("sorry");
-    // }else{
-    //     alert("saldj");
-    // };
+    let wallet = Number(document.querySelector("#wallet").value);
+    let totalbill = Number(document.getElementById('totalbill').innerHTML);
+    if (wallet <= document.getElementById('totalbill').innerHTML) {
+        document.querySelector(".black").style = "display : flex";
+        document.querySelector("#body").style = "overflow: hidden";
+        document.querySelector(".mess").style = "display  : flex";
+        // alert("soty");
+
+
+    } else {
+        appleqty++;
+        console.log(appleqty)
+        document.querySelector('#apple').value = appleqty;
+        total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
+        document.getElementById('totalbill').innerHTML = total;
+        console.log(total);
+    }
+    console.log(wallet < document.getElementById('totalbill').innerHTML)
+
 
 }
 function minusapple() {
@@ -32,12 +81,23 @@ function minusapple() {
 }
 
 function addorange() {
-    orangeqty++;
-    console.log(orangeqty)
-    document.querySelector('#orange').value = orangeqty;
-    total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
-    document.getElementById('totalbill').innerHTML = total;
-    console.log(total);
+    let wallet = Number(document.querySelector("#wallet").value);
+    let totalbill = Number(document.getElementById('totalbill').innerHTML);
+    if (wallet <= document.getElementById('totalbill').innerHTML) {
+        document.querySelector(".black").style = "display : flex";
+        document.querySelector("#body").style = "overflow: hidden";
+        document.querySelector(".mess").style = "display  : flex";
+        // alert("soty");
+
+
+    } else {
+        orangeqty++;
+        console.log(orangeqty)
+        document.querySelector('#orange').value = orangeqty;
+        total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
+        document.getElementById('totalbill').innerHTML = total;
+        console.log(total);
+    }
 
 }
 function minusorange() {
@@ -48,12 +108,23 @@ function minusorange() {
     document.getElementById('totalbill').innerHTML = total;
 }
 function addbanana() {
-    bananaqty++;
-    console.log(bananaqty)
-    document.querySelector('#banana').value = bananaqty;
-    total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
-    document.getElementById('totalbill').innerHTML = total;
-    console.log(total);
+    let wallet = Number(document.querySelector("#wallet").value);
+    let totalbill = Number(document.getElementById('totalbill').innerHTML);
+    if (wallet <= document.getElementById('totalbill').innerHTML) {
+        document.querySelector(".black").style = "display : flex";
+        document.querySelector("#body").style = "overflow: hidden";
+        document.querySelector(".mess").style = "display  : flex";
+        // alert("soty");
+
+
+    } else {
+        bananaqty++;
+        console.log(bananaqty)
+        document.querySelector('#banana').value = bananaqty;
+        total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
+        document.getElementById('totalbill').innerHTML = total;
+        console.log(total);
+    }
 
 }
 function minusbanana() {
@@ -64,12 +135,23 @@ function minusbanana() {
     document.getElementById('totalbill').innerHTML = total;
 }
 function addcucumber() {
-    cucumberqty++;
-    console.log(cucumberqty)
-    document.querySelector('#cucumber').value = cucumberqty;
-    total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
-    document.getElementById('totalbill').innerHTML = total;
-    console.log(total);
+    let wallet = Number(document.querySelector("#wallet").value);
+    let totalbill = Number(document.getElementById('totalbill').innerHTML);
+    if (wallet <= document.getElementById('totalbill').innerHTML) {
+        document.querySelector(".black3").style = "display: flex";
+        document.querySelector("#body").style = "overflow: hidden";
+        document.querySelector(".mess3").style = "display : flex";
+        // alert("soty");
+
+
+    } else {
+        cucumberqty++;
+        console.log(cucumberqty)
+        document.querySelector('#cucumber').value = cucumberqty;
+        total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
+        document.getElementById('totalbill').innerHTML = total;
+        console.log(total);
+    }
 
 }
 function minuscucumber() {
@@ -80,12 +162,23 @@ function minuscucumber() {
     document.getElementById('totalbill').innerHTML = total;
 }
 function addmug() {
-    mugqty++;
-    console.log(mugqty)
-    document.querySelector('#mug').value = mugqty;
-    total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
-    document.getElementById('totalbill').innerHTML = total;
-    console.log(total);
+    let wallet = Number(document.querySelector("#wallet").value);
+    let totalbill = Number(document.getElementById('totalbill').innerHTML);
+    if (wallet <= document.getElementById('totalbill').innerHTML) {
+        document.querySelector(".black3").style = "display: flex";
+        document.querySelector("#body").style = "overflow: hidden";
+        document.querySelector(".mess3").style = "display : flex";
+        // alert("soty");
+
+
+    } else {
+        mugqty++;
+        console.log(mugqty)
+        document.querySelector('#mug').value = mugqty;
+        total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
+        document.getElementById('totalbill').innerHTML = total;
+        console.log(total);
+    }
 
 }
 function minusmug() {
@@ -96,12 +189,23 @@ function minusmug() {
     document.getElementById('totalbill').innerHTML = total;
 }
 function addrose() {
-    roseqty++;
-    console.log(roseqty)
-    document.querySelector('#rose').value = roseqty;
-    total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
-    document.getElementById('totalbill').innerHTML = total;
-    console.log(total);
+    let wallet = Number(document.querySelector("#wallet").value);
+    let totalbill = Number(document.getElementById('totalbill').innerHTML);
+    if (wallet <= document.getElementById('totalbill').innerHTML) {
+        document.querySelector(".black3").style = "display: flex";
+        document.querySelector("#body").style = "overflow: hidden";
+        document.querySelector(".mess3").style = "display : flex";
+        // alert("soty");
+
+
+    } else {
+        roseqty++;
+        console.log(roseqty)
+        document.querySelector('#rose').value = roseqty;
+        total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
+        document.getElementById('totalbill').innerHTML = total;
+        console.log(total);
+    }
 
 }
 function minusrose() {
@@ -111,3 +215,4 @@ function minusrose() {
     total = appleqty * 10 + orangeqty * 15 + bananaqty * 7 + cucumberqty * 12 + mugqty * 130 + roseqty * 80;;
     document.getElementById('totalbill').innerHTML = total;
 }
+
